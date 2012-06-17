63035e1abe28a593d837502d3e1f54fc465ee228
Kevin Gao <kevin.gao@duke.edu>
2012-06-17 12:22:13 -0700

    Adds random string generator
    For now, routing /token to get a new randomly generated 32-character
    token.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express' });
+};
+
+exports.getToken = function(req, res){
+  var util = require('../lib/util');
+  res.json({token: util.randomGenerator()});
 };
