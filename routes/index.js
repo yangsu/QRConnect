d92dd7efafc621362de2c237195cdb4032704cce
Yang Su <suyang1+services@gmail.com>
2012-06-18 09:08:18 -0700

    Added _ to frontend

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ exports.index = function(req, res){
   res.render('index', {
     title: 'Express',
     scripts: [
-      'javascripts/libs/socket.io.js',
       'javascripts/libs/jquery-1.7.2.js',
+      'javascripts/libs/socket.io.js',
+      'javascripts/libs/underscore-1.3.3.js',
       'javascripts/script.js'
     ]
   })
